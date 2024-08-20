b3118cb4d38d3daaf7707c1f55d66dd6a9c55c1c
MrBattka <110553715+MrBattka@users.noreply.github.com>
2024-08-20 18:18:26 +0300

    refactoring code

--- src/components/XiaomiYandexJBL.jsx
+++ src/components/XiaomiYandexJBL.jsx
@@ -2,39 +2,12 @@ import React, { useState } from "react";
 import { changeFlag, returnFixPrice } from "../helpers/fixFlags";
 import { baseFix } from "../helpers/baseFix";
 import style from "./styles.module.css";
+import { copyTable } from "../helpers/copy";
 
 const Xiaomi = ({ fullList }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const arr = [];
-
-  const copyTable = () => {
-    const elTable = document.querySelector("table");
-
-    let range, sel;
-
-    // Ensure that range and selection are supported by the browsers
-    if (document.createRange && window.getSelection) {
-      range = document.createRange();
-      sel = window.getSelection();
-      // unselect any element in the page
-      sel.removeAllRanges();
-
-      try {
-        range.selectNodeContents(elTable);
-        sel.addRange(range);
-      } catch (e) {
-        range.selectNode(elTable);
-        sel.addRange(range);
-      }
-
-      document.execCommand("copy");
-    }
-
-    sel.removeAllRanges();
-
-    console.log("Element Copied! Paste it in a file");
-  };
 
   fullList.map((a) => {
     arr.push({ name: a.Наименование, price: a.Опт });
